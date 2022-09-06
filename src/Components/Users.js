119394285f48ba2a9280556ed1881f941625917f
mhk98 <mohsinkabirseo@gmail.com>
2022-09-06 20:51:14 +0600

    add search field

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -2,21 +2,37 @@ import React, { useEffect, useState } from 'react';
 import User from './User';
 
 const Users = () => {
-    const [users, setUsers] = useState([])
+
+    const [searchText, setSearchText] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+
 
     useEffect( () =>{
         const url = 'https://thawing-crag-43400.herokuapp.com/user'
         fetch(url)
         .then(res => res.json())
-        .then(data => setUsers(data))
-    }, [users])
+        .then(data =>{
+            const match = data.filter(d => d.name.toLowerCase().includes(searchText));
+            setSearchResult(match);
+        } )
+    }, [searchText])
+
+
+    const handleSearchChange = event =>{
+        setSearchText(event.target.value)
+        
+    }
     return (
         <div>
             <h2 className='text-3xl font-bold text-center my-8'>User Information</h2>
 
+            <div className='grid place-items-center my-8 mx-60'>
+                <input onChange={handleSearchChange} className='border-2 rounded w-full h-12 p-2 ' type="text" name="" id="" placeholder='Search'/>
+            </div>
+
             <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
             {
-                users.map(user => <User key={user._id} user={user}></User>)
+                searchResult.map(user => <User key={user._id} user={user}></User>)
             }
             </div>
         </div>
